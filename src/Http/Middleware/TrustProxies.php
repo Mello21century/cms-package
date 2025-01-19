01c35427c2f2ca58b4ddb5d8bee43a78c4b57fb9
Ahmed Safaa <Mello21century@users.noreply.github.com>
2025-01-19 22:15:05 +0300

    Update TrustProxies.php

--- src/Http/Middleware/TrustProxies.php
+++ src/Http/Middleware/TrustProxies.php
@@ -2,7 +2,7 @@
 
 namespace Juzaweb\Http\Middleware;
 
-use Fideloper\Proxy\TrustProxies as Middleware;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
